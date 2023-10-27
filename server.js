1c985d311a135e8918a741325c093a515c7d0fa1
ramteke3yash <ramteke.yashkun@gmail.com>
2023-10-27 21:25:01 +0530

    cors fix

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    credentials: true,
+  })
+);
 
 app.use("/user", userRoutes);
 
